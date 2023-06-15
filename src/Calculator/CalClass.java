f4d026ec28b2a087caa01d87b854b31442b02160
QUOTIENT <tjsgh5965@naver.com>
2023-06-15 15:33:32 +0900

    Initial commit

---
+++ src/Calculator/CalClass.java
@@ -0,0 +1,168 @@
+package Calculator;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class CalClass {
+	public String input;
+	private String value; // 최종값
+	private String cal_formula; // 계산중인 수식
+	private String operator;
+	private boolean isCalculated;
+	
+	public CalClass() {
+		this.input = "0";
+		this.value = "null";
+		this.cal_formula = "";
+		this.operator = "";
+		this.setCalculated(false);
+	}
+	
+	public void reset() {
+		this.input = "0";
+		this.value = "null";
+		this.cal_formula = "";
+		this.operator = "";
+		this.setCalculated(false);
+	}
+	
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+
+	public void setInput(double input) {
+		this.input = this.to_string(input);
+	}
+	
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getCal_formula() {
+		return cal_formula;
+	}
+
+	public void setCal_formula(String cal_formula) {
+		this.cal_formula = cal_formula;
+	}
+	
+	boolean isDouble(double num) {
+		System.out.println(num + " : " + (int)num);
+		if (num != (double) ((int) num)) return true;
+		return false;
+	}
+	
+	boolean isDouble(String num) {
+		double n = this.stod(num);
+		if (n != (double) ((int) n)) return true;
+		return false;
+	}
+	
+	public void setTextField(JTextField textField, String input) {
+		if (this.getInput().charAt(this.getInput().length() - 1) == '.') {
+			if (this.isCalculated()) {
+				this.setInput(this.getInput().replace(".", ""));
+			} else {
+				textField.setText(input);
+				return;
+			}
+		}
+		if (this.isDouble(this.stod(input))) {
+			textField.setText(input);
+		} else {
+			textField.setText(Integer.toString((int)this.stod(input)));
+		}
+		System.out.println(this.getInput());
+	}
+	
+	public void pushInput(int num) {
+		if (this.getInput().equals("0") || this.getInput().equals("") || this.isCalculated()) {
+			this.setInput(this.to_string(num));
+			this.setCalculated(false);
+		}
+		else {
+			this.setInput(this.getInput() + Integer.toString(num));
+		}
+	}
+
+	public void popInput() {
+		this.setInput(this.getInput().substring(0, this.getInput().length() - 1));
+	}
+	public String getOperator() {
+		return operator;
+	}
+
+	public void setOperator(String operator) {
+		this.operator = operator;
+		this.setCal_formula(this.getCal_formula() + " " + this.getInput() + " " + this.getOperator());
+	}
+	
+	public double stod(String str) { return Double.parseDouble(str); }
+	
+	public String to_string(int num) { return Integer.toString(num); }
+	
+	public String to_string(double num) { return Double.toString(num); }
+
+	public boolean isCalculated() {
+		return isCalculated;
+	}
+
+	public void setCalculated(boolean isCalculated) {
+		this.isCalculated = isCalculated;
+	}
+	
+	public void calculate(String operator) {
+		
+		if (this.isCalculated()) { 
+			this.setOperator(operator);
+			return;
+		}
+		
+		if (this.getValue().equals("null")) {
+			this.setValue(this.getInput());
+			this.setCalculated(true);
+			this.setOperator(operator);
+			return;
+		}
+		
+		if (this.getOperator().equals("＋")) {
+			this.setValue(this.to_string(this.stod(this.getInput()) + this.stod(this.getValue())));
+			//System.out.println(this.stod(this.getInput()) + "+" + this.stod(this.getValue()));
+			this.setCalculated(true);
+		} else if (this.getOperator().equals("－")) {
+			this.setValue(this.to_string(this.stod(this.getInput()) - this.stod(this.getValue())));
+			//System.out.println(this.stod(this.getInput()) + "-" + this.stod(this.getValue()));
+			this.setCalculated(true);
+			
+		} else if (this.getOperator().equals("×")) {
+			this.setValue(this.to_string(this.stod(this.getInput()) * this.stod(this.getValue())));
+			//System.out.println(this.stod(this.getInput()) + "x" + this.stod(this.getValue()));
+			this.setCalculated(true);
+			
+		} else if (this.getOperator().equals("÷")) {
+			this.setValue(this.to_string(this.stod(this.getInput()) / this.stod(this.getValue())));
+			//System.out.println(this.stod(this.getInput()) + "/" + this.stod(this.getValue()));
+			this.setCalculated(true);
+		}
+		this.setOperator(operator);
+	}
+	
+}
---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,157 @@
+package Calculator;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Calculator extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Calculator frame = new Calculator();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Calculator() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 350, 400);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		textField = new JTextField();
+		textField.setEditable(false);
+		textField.setFont(new Font("Arial", Font.BOLD, 45));
+		textField.setHorizontalAlignment(SwingConstants.RIGHT);
+		textField.setText("0");
+		textField.setBounds(0, 10, 334, 70);
+		contentPane.add(textField);
+		contentPane.setLayout(null);
+
+		CalClass cal = new CalClass();
+
+		String[] btn_names = {"%", "C", "CE", "DE", "÷",
+							  "√", "7", "8" , "9" , "×",
+							  "x²", "4", "5", "6" , "－",
+							  "x³", "1", "2", "3" , "＋",
+							  "¹⁄ₓ", "±", "0", ".", "=" };
+		JButton[] btnArr = new JButton[btn_names.length];
+		
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				int index = i * 5 + j;
+				btnArr[index] = new JButton(btn_names[index]);
+				btnArr[index].setBounds(50 + (50 * j), 100 + (50 * i), 50, 50);
+				
+				btnArr[index].addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						String btn_name = e.getActionCommand();
+						if (btn_name.equals("%")) {
+							cal.setOperator("%");
+						} else if (btn_name.equals("C")) {
+							cal.setInput(cal.to_string(0));
+							cal.setTextField(textField, cal.getInput());
+						} else if (btn_name.equals("CE")) {
+							cal.reset();
+							cal.setTextField(textField, cal.getInput());
+						} else if (btn_name.equals("DE")) {
+							cal.popInput();
+						} else if (btn_name.equals("÷")) {
+
+							//--------------------
+							cal.calculate("÷");
+							cal.setTextField(textField, cal.getValue());
+							
+						} else if (btn_name.equals("√")) {
+							cal.setCalculated(true);
+							cal.setInput(Math.sqrt(cal.stod(cal.getInput())));
+							cal.setValue(cal.getInput());
+							cal.setTextField(textField, cal.getInput());
+						} else if (btn_name.equals("×")) {
+
+							//--------------------
+							cal.calculate("×");
+							cal.setTextField(textField, cal.getValue());
+							
+						} else if (btn_name.equals("x²")) {
+							cal.setCalculated(true);
+							cal.setInput(Math.pow(cal.stod(cal.getInput()), 2));
+							cal.setValue(cal.getInput());
+							cal.setTextField(textField, cal.getInput());
+						} else if (btn_name.equals("－")) {
+							
+							//--------------------
+							cal.calculate("－");
+							cal.setTextField(textField, cal.getValue());
+							
+						} else if (btn_name.equals("x³")) {
+							cal.setCalculated(true);
+							cal.setInput(Math.pow(cal.stod(cal.getInput()), 3));
+							cal.setValue(cal.getInput());
+							cal.setTextField(textField, cal.getInput());
+						} else if (btn_name.equals("＋")) {
+
+							//--------------------
+							cal.calculate("＋");
+							cal.setTextField(textField, cal.getValue());
+							
+						} else if (btn_name.equals("¹⁄ₓ")) {
+							cal.setCalculated(true);
+							cal.setInput(1.0 / cal.stod(cal.getInput()));
+							cal.setValue(cal.getInput());
+							cal.setTextField(textField, cal.getInput());
+						} else if (btn_name.equals("±")) {
+							if (cal.stod(cal.getInput()) != Double.parseDouble("0")) {
+								if (cal.getInput().contains("-")) {
+									cal.setInput(cal.getInput().replace("-", ""));
+								} else {
+									cal.setInput("-" + cal.getInput());
+								}
+								cal.setTextField(textField, cal.getInput());
+							}
+						} else if (btn_name.equals(".")) {
+							if (!cal.getInput().contains(".")) {
+								cal.setInput(cal.getInput() + ".");
+								cal.setTextField(textField, cal.getInput());
+							}
+						} else if (btn_name.equals("=")) {
+							
+						} else {
+							cal.pushInput(Integer.parseInt(e.getActionCommand()));
+							cal.setTextField(textField, cal.getInput());
+						}
+					}
+				});
+				
+				contentPane.add(btnArr[index]);
+			}
+		}
+	}
+}
